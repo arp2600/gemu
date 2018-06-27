3d398a90e86eb1ede8e74794d11ab33632daae07
George Roger <georger@xmos.com>
2018-06-27 23:18:37 +0100

    Modified sdl2 example to make suitable for emulator work.

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,59 @@ extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::render::TextureAccess;
+use sdl2::pixels::PixelFormatEnum;
 
-fn main() {
+pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+
+    let scale = 5;
+
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 800, 600)
+        .window("rust-sdl2 demo: Video", 160 * scale, 144 * scale)
         .position_centered()
+        .opengl()
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().software().build().unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
     let texture_creator = canvas.texture_creator();
-    let mut texture = texture_creator
-        .create_texture(None, TextureAccess::Streaming, 32, 32)
-        .unwrap();
-    let mut pixels = [0; 32*32*4];
-    for i in 0..32 {
-        pixels[i*32*4 + 0] = 0xff;
-        pixels[i*32*4 + 1] = 0xff;
-        pixels[i*32*4 + 2] = 0xff;
-        pixels[i*32*4 + 3] = 0xff;
-    }
-    texture.update(None, &pixels, 32*4).unwrap();
 
-    canvas.copy(&texture, None, None).expect("Render failed");
+    let mut texture = texture_creator
+        .create_texture_streaming(PixelFormatEnum::RGB24, 160, 144)
+        .unwrap();
+    // Create a checker board pattern
+    texture
+        .with_lock(None, |buffer: &mut [u8], pitch: usize| {
+            for y in 0..144 {
+                for x in 0..160 {
+                    let offset = y * pitch + x * 3;
+                    let v = ((x + y) % 2) as u8 * 0xff;
+                    buffer[offset] = v;
+                    buffer[offset + 1] = v;
+                    buffer[offset + 2] = v;
+                }
+            }
+        })
+        .unwrap();
+
+    canvas.clear();
+    canvas.copy(&texture, None, None).unwrap();
     canvas.present();
 
-    'mainloop: loop {
-        for event in sdl_context.event_pump().unwrap().poll_iter() {
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
-                    keycode: Option::Some(Keycode::Escape),
+                    keycode: Some(Keycode::Escape),
                     ..
-                } => break 'mainloop,
+                } => break 'running,
                 _ => {}
             }
         }
+        // The rest of the game loop goes here...
     }
 }
