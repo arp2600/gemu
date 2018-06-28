17628f9be4e860171da5b13c86de6160927c9daa
George Roger <georger@xmos.com>
2018-06-28 21:00:26 +0100

    Got gameboy emulator drawing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
+extern crate gb_emu;
 extern crate sdl2;
 
+use gb_emu::Emulator;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
+use std::thread::sleep;
+use std::time::{Duration, SystemTime};
+
+struct FrameTimer {
+    start: SystemTime,
+    frame_time: u32,
+}
+
+impl FrameTimer {
+    fn new(frame_time_nanos: u32) -> FrameTimer {
+        FrameTimer {
+            start: SystemTime::now(),
+            frame_time: frame_time_nanos,
+        }
+    }
+
+    fn sleep_till_end_of_frame(&self) {
+        let frame_time = Duration::new(0, self.frame_time);
+        let sleep_time = match self.start.elapsed() {
+            Ok(x) => if frame_time > x {
+                frame_time - x
+            } else {
+                Duration::new(0, 0)
+            },
+            Err(_) => Duration::new(0, 0),
+        };
+        sleep(sleep_time);
+    }
+}
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -44,7 +75,13 @@ pub fn main() {
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
+    let cartridge_rom = "../ROMs/tetris.gb";
+    let boot_rom = Some("../ROMs/dmg_rom.gb");
+    let mut emulator = Emulator::new(boot_rom, cartridge_rom);
+
     'running: loop {
+        let frame_timer = FrameTimer::new(1000000000 / 60);
+
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -55,6 +92,27 @@ pub fn main() {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
+
+        emulator.run_until_vblank();
+        texture
+            .with_lock(None, |buffer: &mut [u8], pitch: usize| {
+                let screen_buffer = emulator.get_screen_buffer();
+                for y in 0..144 {
+                    for x in 0..160 {
+                        let offset = y * pitch + x * 3;
+                        // let v = ((x + y) % 2) as u8 * 0xff;
+                        let v = screen_buffer[y * 160 + x] * 85;
+                        buffer[offset] = v;
+                        buffer[offset + 1] = v;
+                        buffer[offset + 2] = v;
+                    }
+                }
+            })
+            .unwrap();
+
+        canvas.copy(&texture, None, None).unwrap();
+        canvas.present();
+
+        frame_timer.sleep_till_end_of_frame();
     }
 }
