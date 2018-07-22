e40825dc95bb94a9e3cbb93810b7b8afa51a42a1
George Roger <georger@xmos.com>
2018-07-22 23:48:12 +0100

    Updating to latest working gb_emu

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use gb_emu::{Command, Emulator, JoyPad};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
+use std::env;
 
 fn set_button(joypad: &mut JoyPad, keycode: Keycode, state: bool) {
     match keycode {
@@ -23,6 +24,17 @@ fn set_button(joypad: &mut JoyPad, keycode: Keycode, state: bool) {
 }
 
 pub fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("error: add rom file as argument");
+        eprintln!("usage `cargo run -- <rom_file>`");
+        return;
+    }
+
+    let cartridge_rom = &args[1];
+    // println!("args: {:?}", args);
+    // return;
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
@@ -38,11 +50,18 @@ pub fn main() {
     let texture_creator = canvas.texture_creator();
 
     let mut emulator = {
-        let cartridge_rom = "../ROMs/tetris.gb";
+        // let cartridge_rom = "../ROMs/tetris.gb";
+        // let cartridge_rom = "../ROMs/dr_mario.gb";
+        // let cartridge_rom = "../ROMs/rocky_and_bullwinkle.gb";
+        // let cartridge_rom = "../ROMs/super_mario_land_1.1.gb";
+        // let cartridge_rom = "../ROMs/ultima_runes_of_virtue.gb";
+        // let cartridge_rom = "../ROMs/pokemon_blue.gb";
+        // let cartridge_rom = "gb_emu/gb-test-roms/cpu_instrs/cpu_instrs.gb";
         // let boot_rom = Some("../ROMs/dmg_rom.gb");
         let boot_rom = None;
-        Emulator::new(boot_rom, cartridge_rom)
+        Emulator::new(boot_rom, &cartridge_rom)
     };
+    // emulator.set_tracing(true);
 
     let draw_fn = {
         let mut texture = texture_creator
@@ -55,10 +74,28 @@ pub fn main() {
             let y = usize::from(line_index);
             for (x, v) in line.iter().enumerate() {
                 let offset = y * 160 * 3 + x * 3;
-                let v = *v * 85;
-                texture_buffer[offset] = v;
-                texture_buffer[offset + 1] = v;
-                texture_buffer[offset + 2] = v;
+                match *v {
+                    0...3 => {
+                        let v = *v * 85;
+                        // let v = *v * 15;
+                        texture_buffer[offset] = v;
+                        texture_buffer[offset + 1] = v;
+                        texture_buffer[offset + 2] = v;
+                    }
+                    // 4...7 => {
+                    //     let v = ((*v - 4) * 25) + 150;
+                    //     texture_buffer[offset] = v;
+                    //     texture_buffer[offset + 1] = 0;
+                    //     texture_buffer[offset + 2] = 0;
+                    // }
+                    // 8...11 => {
+                    //     let v = ((*v - 8) * 25) + 150;
+                    //     texture_buffer[offset] = 0;
+                    //     texture_buffer[offset + 1] = 0;
+                    //     texture_buffer[offset + 2] = v;
+                    // }
+                    _ => panic!("error: bad color value!"),
+                }
             }
 
             if line_index == 143 {
@@ -80,7 +117,9 @@ pub fn main() {
                     | Event::KeyDown {
                         keycode: Some(Keycode::Escape),
                         ..
-                    } => return Command::Stop,
+                    } => {
+                        return Command::Stop;
+                    }
                     Event::KeyDown {
                         keycode: Some(x), ..
                     } => set_button(joypad, x, true),
